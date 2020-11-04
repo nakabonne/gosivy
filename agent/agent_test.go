b71ae8ed273baec9a014bc5b09aa1996fb081f94
nakabonne <ryo@nakao.dev>
2020-11-04 21:32:14 +0900

    Ensure to handle signal properly

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -1,10 +1,13 @@
 package agent
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/nakabonne/gosivy/stats"
 )
 
 func TestListenAndClose(t *testing.T) {
@@ -15,4 +18,35 @@ func TestListenAndClose(t *testing.T) {
 
 	assert.True(t, os.IsNotExist(err))
 	assert.Empty(t, pidFile)
+}
+
+func TestHandle(t *testing.T) {
+	tests := []struct {
+		name    string
+		signal  byte
+		wantErr bool
+	}{
+		{
+			name:    "signal meta received",
+			signal:  stats.SignalMeta,
+			wantErr: false,
+		},
+		{
+			name:    "signal stats received",
+			signal:  stats.SignalStats,
+			wantErr: false,
+		},
+		{
+			name:    "unknown signal received",
+			signal:  byte(0x9),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := new(bytes.Buffer)
+			err := handle(b, []byte{tt.signal})
+			assert.Equal(t, tt.wantErr, err != nil)
+		})
+	}
 }
