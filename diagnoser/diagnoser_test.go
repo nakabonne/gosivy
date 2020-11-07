4a9e7d8b110a39f8077ae97b10763e30a8e9e975
Ryo Nakao <ryo@nakao.dev>
2020-11-07 11:09:08 +0900

    Garantee to perform scraper correctly (#12)

--- diagnoser/diagnoser_test.go
+++ diagnoser/diagnoser_test.go
@@ -2,30 +2,47 @@ package diagnoser
 
 import (
 	"context"
+	"encoding/json"
 	"net"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/nakabonne/gosivy/stats"
 )
 
 func TestStartScraping(t *testing.T) {
-	type args struct {
-		ctx      context.Context
-		addr     *net.TCPAddr
-		interval time.Duration
-		statsCh  chan<- *stats.Stats
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	addr := startServer()
+
+	_, err := startScraping(ctx, addr, time.Microsecond, make(chan<- *stats.Stats, 100))
+	time.Sleep(100 * time.Millisecond)
+	assert.Nil(t, err)
+}
+
+func startServer() *net.TCPAddr {
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
 	}
-	tests := []struct {
-		name    string
-		args    args
-		want    *stats.Meta
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-		})
-	}
+	go func() {
+		defer ln.Close()
+		for {
+			conn, _ := ln.Accept()
+			sig := make([]byte, 1)
+			_, _ = conn.Read(sig)
+			switch sig[0] {
+			case stats.SignalMeta:
+				b, _ := json.Marshal(&stats.Meta{})
+				_, _ = conn.Write(b)
+			case stats.SignalStats:
+				b, _ := json.Marshal(&stats.Stats{})
+				_, _ = conn.Write(b)
+			}
+			conn.Close()
+		}
+	}()
+	return ln.Addr().(*net.TCPAddr)
 }
